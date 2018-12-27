638727666a29f8bb76ccaa340c78ff78d3402e71
Adam Światek <adasko.86@gmail.com>
2018-12-27 10:00:14 +0100

    fla-pi v1.0

--- main.js
+++ main.js
@@ -3,20 +3,17 @@ var mainState = {
     preload: function() { 
         // This function will be executed at the beginning     
         // That's where we load the images and sounds 
-        game.load.image('cloud1', 'assets/cloud1.png');
-        game.load.image('cloud2', 'assets/cloud2.png');
-        game.load.image('cloud3', 'assets/cloud3.png');
-        game.load.image('bird', 'assets/bird2.png');
         game.load.image('pipe', 'assets/pipe.png');
         game.load.image('pipe_end_top', 'assets/pipe_end_top.png');
         game.load.image('pipe_end_bottom', 'assets/pipe_end_bottom.png');
-        game.load.audio('jump', 'assets/jump.wav'); 
+        game.load.audio('jump', 'assets/jump.wav');
     },
 
-    create: function() { 
+    create: function () { 
+        
         // This function is called after the preload function     
         // Here we set up the game, display sprites, etc. 
-        game.stage.backgroundColor = '#71c5cf';
+        game.stage.backgroundColor = '#00b9f2';
         
         //set physics
         game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -41,7 +38,11 @@ var mainState = {
         
         // Call the 'jump' function when the spacekey is hit
         var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        spaceKey.onDown.add(this.jump, this);         
+        spaceKey.onDown.add(this.jump, this);      
+
+        var escKey = game.input.keyboard.addKey(Phaser.Keyboard.ESC);
+        escKey.onDown.add(this.escButton, this);
+
         game.input.onDown.add(this.jump, this);
         
         // Create an empty group pipes
@@ -61,8 +62,8 @@ var mainState = {
         this.bird.anchor.setTo(-0.2, 0.5); 
         
         //Load jump audio
-        this.jumpSound = game.add.audio('jump'); 
-        console.log("lipa");
+        this.jumpSound = game.add.audio('jump');
+        this.jumpSound.volume = 0.1; 
     },
 
     update: function() {
@@ -85,9 +86,16 @@ var mainState = {
         
         game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this);  
     },
-    
+
+    //pause menu
+    escButton: function () {
+        game.physics.arcade.isPaused = (game.physics.arcade.isPaused) ? false : true;
+    },
+
     // Make the bird jump 
-    jump: function() {
+    jump: function () {
+        if (game.physics.arcade.isPaused)
+            return;
         // Add a vertical velocity to the bird
         this.bird.body.velocity.y = -350;
         
@@ -101,7 +109,9 @@ var mainState = {
         animation.start(); 
         
         //play jump melody
-        this.jumpSound.play();
+        if (isMusicPlay) {
+            this.jumpSound.play();
+        }
         if (this.bird.alive == false) return; 
     },
 
@@ -137,7 +147,9 @@ var mainState = {
         pipe.outOfBoundsKill = true;
     },
     
-    addRowOfPipes: function() {
+    addRowOfPipes: function () {
+        if (game.physics.arcade.isPaused)
+            return;
         // Randomly pick a number between 1 and 5
         // This will be the hole position
         var hole = Math.floor(Math.random() * 5) + 1;
@@ -161,6 +173,8 @@ var mainState = {
     },
 
     addCloud: function () {
+        if (game.physics.arcade.isPaused)
+            return;
         var cloud = game.add.sprite(400, Math.floor(Math.random() * 400), 'cloud3');
         // Add the pipe to our previously created group
         this.clouds.add(cloud);
@@ -177,6 +191,8 @@ var mainState = {
     },
 
     addCloudSmall: function () {
+        if (game.physics.arcade.isPaused)
+            return;
         var cloud = game.add.sprite(400, Math.floor(Math.random() * 400), 'cloud1');
         // Add the pipe to our previously created group
         this.clouds.add(cloud);
@@ -193,6 +209,8 @@ var mainState = {
     },
 
     addCloudBig: function () {
+        if (game.physics.arcade.isPaused)
+            return;
         var cloud = game.add.sprite(400, Math.floor(Math.random() * 400), 'cloud2');
         // Add the pipe to our previously created group
         this.clouds.add(cloud);
@@ -227,32 +245,64 @@ var mainState = {
     }, 
 };
 
+var btnMusicOff;
+var btnMusicOn;
+var backgroundMusic;
+var isMusicPlay;
+var creditsMain;
+var scoreList;
+
 mainMenu = {
     preload: function() {
         // This function will be executed at the beginning     
         // That's where we load the images and sounds 
-        game.load.spritesheet('play_button', 'assets/play_button.png', 180, 128);
+        game.load.spritesheet('play_button', 'assets/play_button.png', 119, 74);
+       // game.load.spritesheet('score_button', 'assets/score_button.png', 180, 128);
         game.load.image('cloud1', 'assets/cloud1.png');
         game.load.image('cloud2', 'assets/cloud2.png');
         game.load.image('cloud3', 'assets/cloud3.png');
         game.load.image('bird', 'assets/bird2.png');
+        game.load.image('music_on_icon', 'assets/music_on_icon.png');
+        game.load.image('music_off_icon', 'assets/music_off_icon.png');
+
+        game.load.audio('background_music', 'assets/sunnyDayWhistler.mp3');
     },
 
     create: function () {
-
         // This function is called after the preload function     
         // Here we set up the game, display sprites, etc. 
-        game.stage.backgroundColor = '#71c5cf';
-
+        game.stage.backgroundColor = '#00b9f2';
+        if (!isMusicLoaded) {
+            this.loadMusic();
+            isMusicLoaded = true;
+        }
         this.clouds = game.add.group();
         this.timer = game.time.events.loop(15000, this.addCloudSmall, this);
         this.timer = game.time.events.loop(10500, this.addCloud, this);
         this.timer = game.time.events.loop(8500, this.addCloudBig, this);
         this.addCloudBig();
 
+        btnMusicOff = game.add.button(370, 10, 'music_off_icon', this.onOffMusicClick, this, 0, 1, 0);
+        btnMusicOn = game.add.button(370, 10, 'music_on_icon', this.onOffMusicClick, this, 0, 1, 0);
+        if (isMusicPlay) {
+            btnMusicOn.visible = false;
+        } else {
+            btnMusicOff.visible = false;
+        }
 
         var btnPlay = game.add.button(game.world.centerX, 360, 'play_button', this.playClick, this, 0, 1, 0);
         btnPlay.anchor.set(0.5, 0.5);
+
+
+        //this.labelScore = game.add.text(game.world.centerX, 420, "SCORE", { font: "30px Agency FB", fontWeight: "bold", fill: "#ffffff" });
+        //this.labelScore.anchor.set(0.5, 0.5);
+        //this.labelScore.inputEnabled = true;
+        //this.labelScore.events.onInputDown.add(this.showScore, this);
+
+        this.labelCredits = game.add.text(game.world.centerX, 420, "CREDITS", { font: "24px Agency FB", fontWeight: "bold", fill: "#ffffff" });
+        this.labelCredits.anchor.set(0.5, 0.5);
+        this.labelCredits.inputEnabled = true;
+        this.labelCredits.events.onInputDown.add(this.showCredits, this);
 
         //set physics
         game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -295,7 +345,71 @@ mainMenu = {
         game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this);
     },
 
+    loadMusic: function () {
+        //to play music in background and loop them
+        backgroundMusic = game.add.audio('background_music');
+        backgroundMusic.loop = true; // This is what you are lookig for
+        backgroundMusic.volume = 0.5;
+        backgroundMusic.play();
+        isMusicPlay = true;
+    },
+
+    showScore: function () {
+        console.log("score");
+
+        if (this.scoreList == null) {
+            var scoreList = "       SCORE:";
+            for (var i = 1; i <= 8; i++) {
+                scoreList += this.genereateScoreString(i);
+            }
+            this.scoreList = game.add.text(game.world.centerX, 10, scoreList, { font: "24px Agency FB", fontWeight: "bold", fill: "#595b5c", backgroundColor: "#00b9f2" });
+            this.scoreList.anchor.set(0.5, 0);
+        } else {
+            this.hideScore();
+        }
+    },
+
+    genereateScoreString: function (number) {
+        return "\r\n       " + number + ". Adam " + number + "         ";
+    },
+
+    hideScore: function () {
+        if (this.creditsMain != null)
+            this.creditsMain.destroy();
+        this.creditsMain = null
+    },
+
+    showCredits: function () {
+        if (this.creditsMain == null) {
+            this.creditsMain = game.add.text(game.world.centerX, 150, "           Fla-PI\r\n Game created by:\r\n    Adam Swiatek\r\n\r\n", { font: "24px Agency FB", fontWeight: "bold", fill: "#ffffff" });
+            this.creditsMain.anchor.set(0.5, 0.5);
+        } else{
+            this.hideCredits();
+        }
+    },
+
+    hideCredits: function () {
+        if (this.creditsMain != null)
+            this.creditsMain.destroy();
+        this.creditsMain = null
+    },
+
+    onOffMusicClick: function () {
+        if (btnMusicOn.visible) {
+            backgroundMusic.play();
+            btnMusicOn.visible = false;
+            btnMusicOff.visible = true;
+            isMusicPlay = true;
+        } else {
+            backgroundMusic.stop();
+            btnMusicOff.visible = false;
+            btnMusicOn.visible = true;
+            isMusicPlay = false;
+        }
+    },
+
     playClick: function () {
+        this.hideCredits();
         this.gameStarted = true;
         //game.state.stop('main');
         // Start the state to actually start the game
@@ -366,8 +480,12 @@ mainMenu = {
     },
 }
 
-// Initialize Phaser, and create a 400px by 490px game
-var game = new Phaser.Game(400, 490);
+    var isMusicLoaded = false;
+    // Initialize Phaser, and create a 400px by 490px game
+    var game = new Phaser.Game(400, 490);
+
+    //to create transparent background
+    //var game = new Phaser.Game(400, 490, Phaser.AUTO, "", null, true, true, null);
 
     // Add the 'mainState' and call it 'main'
     game.state.add('game', mainState);
